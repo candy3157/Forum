661c4294f6e16db198e94e7ee756d0cf1a851977
candy3157 <tkwk9731@gmail.com>
2026-01-15 01:01:04 +0900

    asdf

---
+++ forum-app/src/lib/prisma.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const globalForPrisma = globalThis;
+
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        adapter: new PrismaPg(
+            new Pool({
+                connectionString: process.env.DATABASE_URL,
+            })
+        ),
+    });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
